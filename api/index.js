8f1d567bc79a699690656f89808f783cf99b449a
dobutamine <tim.antonius@outlook.com>
2021-08-02 22:09:03 +0200

    added lab view

--- api/index.js
+++ api/index.js
@@ -1,108 +1,105 @@
-const { Monitor } = require('./models/monitor')
-const express = require('express')
-const ws = require('ws');
+const { Monitor } = require("./models/monitor");
+const express = require("express");
+const ws = require("ws");
 const app = express();
 
 // import the database scripts
 const db = require("./db");
-db.connect()
+db.connect();
 
 // import the routes
-const users = require('./routes/users')
-const auth = require('./routes/auth')
-const configs = require('./routes/configs')
-const medias = require('./routes/medias');
-const labs = require('./routes/labs')
-const states = require('./routes/states')
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const configs = require("./routes/configs");
+const medias = require("./routes/medias");
+const labs = require("./routes/labs");
+const states = require("./routes/states");
 
-const { createWebSocketStream } = require('ws');
+const { createWebSocketStream } = require("ws");
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // use som of the inbuild middleware
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.static("public"));
 
 // register the routes
-app.use('/api/users', users)
-app.use('/api/auth', auth)
-app.use('/api/configs', configs)
-app.use('/api/media', medias)
-app.use('/api/labs', labs)
-app.use('/api/states', states)
-
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/configs", configs);
+app.use("/api/media", medias);
+app.use("/api/labs", labs);
+app.use("/api/states", states);
 
 // define the port to 3000 if not set by an enviroinment variable
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 // Set up a websocket server
 const wsServer = new ws.Server({ noServer: true });
 
 // start the express server
 const server = app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+  console.log(`Server is listening on port ${port}`);
+});
 
 // handle the request to upgrade the connection to a websocket
-server.on('upgrade', (request, socket, head) => {
-  wsServer.handleUpgrade(request, socket, head, socket => {
-    wsServer.emit('connection', socket, request);
+server.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
   });
 });
 
 // handle the websocket server requests
-wsServer.on('connection', async socket => {
+wsServer.on("connection", async (socket) => {
   // on message
   try {
-    socket.on('message', async message => {
-      const convertedMessage = JSON.parse(message)
+    socket.on("message", async (message) => {
+      const convertedMessage = JSON.parse(message);
       switch (convertedMessage.command) {
-        case 'get':
+        case "get":
           try {
-            let monitor = await Monitor.findOne( { id: convertedMessage.id })
+            let monitor = await Monitor.findOne({ id: convertedMessage.id });
             if (monitor) {
-              socket.send(JSON.stringify(monitor))
+              socket.send(JSON.stringify(monitor));
             }
-          } catch(err) {
-          }
-          break
-        case 'set':
+          } catch (err) {}
+          break;
+        case "set":
           try {
-            let monitor2 = await Monitor.findOne( { id: convertedMessage.id })
-  
+            let monitor2 = await Monitor.findOne({ id: convertedMessage.id });
+
             if (monitor2) {
-              monitor2.heartrate = convertedMessage.heartrate
-              monitor2.satPre = convertedMessage.satPre
-              monitor2.satPost = convertedMessage.satPost
-              monitor2.satVen = convertedMessage.satVen
-              monitor2.respRate = convertedMessage.respRate
-              monitor2.etco2 = convertedMessage.etco2
-              monitor2.abpSyst = convertedMessage.abpSyst
-              monitor2.abpDiast = convertedMessage.abpDiast
-              monitor2.pfi = convertedMessage.pfi
-              monitor2.temp = convertedMessage.temp
-              monitor2.cvp = convertedMessage.cvp
-              monitor2.papSyst = convertedMessage.papSyst
-              monitor2.papDiast = convertedMessage.papDiast
-              monitor2.rhythmType = convertedMessage.rhythmType
-              monitor2.rhythmParameter = convertedMessage.rhythmParameter
-              monitor2.intubated = convertedMessage.intubated
-              monitor2.imageName = convertedMessage.imageName
-              await monitor2.save()
+              monitor2.heartrate = convertedMessage.heartrate;
+              monitor2.satPre = convertedMessage.satPre;
+              monitor2.satPost = convertedMessage.satPost;
+              monitor2.satVen = convertedMessage.satVen;
+              monitor2.respRate = convertedMessage.respRate;
+              monitor2.etco2 = convertedMessage.etco2;
+              monitor2.abpSyst = convertedMessage.abpSyst;
+              monitor2.abpDiast = convertedMessage.abpDiast;
+              monitor2.pfi = convertedMessage.pfi;
+              monitor2.temp = convertedMessage.temp;
+              monitor2.cvp = convertedMessage.cvp;
+              monitor2.papSyst = convertedMessage.papSyst;
+              monitor2.papDiast = convertedMessage.papDiast;
+              monitor2.rhythmType = convertedMessage.rhythmType;
+              monitor2.rhythmParameter = convertedMessage.rhythmParameter;
+              monitor2.intubated = convertedMessage.intubated;
+              monitor2.imageName = convertedMessage.imageName;
+              await monitor2.save();
             }
-            } catch (err) {
-            }
-          break
+          } catch (err) {}
+          break;
       }
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  
 });
-
-
--- web/src/components/LabView.vue
+++ web/src/components/LabView.vue
@@ -1,20 +1,126 @@
 <template>
   <q-dialog v-model="confirm" ref="dialog" persistent buttons>
-    <q-card class="q-ma-sm" style="width: 250px">
-      <div class="q-ma-sm"></div>
-      <q-card-section align="center">
-        <q-select
-          v-model="selectedLabTest"
-          :options="labTestList"
-          style="max-width: 300px"
-          stack-label
-          label="chest compressions pattern"
-        />
+    <q-card class="q-ma-sm" style="width: 450px">
+      <q-card-section>
+        <div>
+          <q-select
+            v-model="selectedLabTest"
+            :options="labTestList"
+            stack-label
+            label="laboratory category"
+          />
+        </div>
+      </q-card-section>
+
+      <q-card-section class="text-grey-6">
+        <div class="row">
+          <div class="col text-left">
+            sodium
+          </div>
+          <div class="col text-center text-grey-11">
+            {{ sodium }}
+          </div>
+          <div class="col text-left">
+            mmol/l
+          </div>
+          <div class="col text-left">
+            pH
+          </div>
+          <div class="col text-center text-grey-11">
+            {{ ph }}
+          </div>
+          <div class="col ext-left">
+            -
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col text-left">
+            potassium
+          </div>
+          <div class="col text-center text-grey-11">
+            {{ potassium }}
+          </div>
+          <div class="col text-left">
+            mmol/l
+          </div>
+          <div class="col text-left">
+            paO2
+          </div>
+          <div class="col text-center text-grey-11">
+            {{ po2 }}
+          </div>
+          <div class="col text-left">
+            mmHg
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col text-left">
+            chloride
+          </div>
+          <div class="col text-center text-grey-11">
+            {{ chloride }}
+          </div>
+          <div class="col text-left">
+            mmol/l
+          </div>
+          <div class="col text-left">
+            paCO2
+          </div>
+          <div class="col text-center text-grey-11">
+            {{ pco2 }}
+          </div>
+          <div class="col text-left">
+            mmHg
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col text-left">
+            glucose
+          </div>
+          <div class="col text-center text-grey-11">
+            {{ glucose }}
+          </div>
+          <div class="col text-left">
+            mmol/l
+          </div>
+          <div class="col text-left">
+            HCO3-
+          </div>
+          <div class="col text-center text-grey-11">
+            {{ bic }}
+          </div>
+          <div class="col text-left">
+            mmol/l
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col text-left">
+            lactate
+          </div>
+          <div class="col text-center text-grey-11">
+            {{ lactate }}
+          </div>
+          <div class="col text-left">
+            mmol/l
+          </div>
+          <div class="col text-left">
+            be
+          </div>
+          <div class="col text-center text-grey-11">
+            {{ be }}
+          </div>
+          <div class="col text-left">
+            mmol/l
+          </div>
+        </div>
       </q-card-section>
 
       <q-card-actions align="center">
-        <q-btn color="secondary" label="Admit" size="sm" @click="onOKClick" />
-        <q-btn color="secondary" label="Cancel" size="sm" @click="onOKClick" />
+        <q-btn color="red-10" label="Close" size="sm" @click="onOKClick" />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -35,28 +141,22 @@ export default {
     return {
       id: "",
       url: "",
+      ph: 7.4,
+      pco2: 35,
+      po2: 100,
+      bic: 25,
+      be: -0.5,
+      glucose: 3.5,
+      lactate: 1.0,
+      sodium: 140,
+      potassium: 3.8,
+      chloride: 100,
       confirm: false,
       selectedLabTest: "bloodgas",
-      labTestList: [
-        "bloodgas",
-        "electrolytes",
-        "complete blood count",
-        "inflammation markers",
-        "metabolic"
-      ]
+      labTestList: ["bloodgas"]
     };
   },
   methods: {
-    getLabSettingsFromServer() {
-      axios
-        .get(`http://localhost:8080/api/labs?id=${this.id}`)
-        .then(res => {
-          console.log(res);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
     show() {
       this.$refs.dialog.show();
     },
@@ -69,6 +169,26 @@ export default {
     },
     onCancelClick() {
       this.hide();
+    },
+    getLabSettingsFromServer() {
+      axios
+        .get(`http://localhost:8080/api/labs?id=${this.id}`)
+        .then(res => {
+          const parsedBloodgas = JSON.parse(res.data.bloodgas);
+          this.sodium = parsedBloodgas.na;
+          this.potassium = parsedBloodgas.k;
+          this.chloride = parsedBloodgas.cl;
+          this.glucose = parsedBloodgas.glucose;
+          this.lactate = parsedBloodgas.lactate;
+          this.ph = parsedBloodgas.ph;
+          this.pco2 = parsedBloodgas.pco2;
+          this.po2 = parsedBloodgas.po2;
+          this.be = parsedBloodgas.be;
+          this.bic = parsedBloodgas.bic;
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   },
   mounted() {
